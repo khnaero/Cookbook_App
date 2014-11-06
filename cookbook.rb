693b4f7daf74712d2724135a4748be1e6b9bee69
Khoi Nguyen <khnaero@gmail.com>
2014-11-06 12:47:59 -0600

    Initial commit

---
+++ cookbook.rb
@@ -0,0 +1,15 @@
+class Cookbook
+
+	def initialize(title)
+		@title = title
+	end
+end
+
+class Recipe
+	def initialize(title, ingredients, steps)
+	end
+end
+
+
+mex_cuisine = Cookbook.new("Mexican Cooking")
+
